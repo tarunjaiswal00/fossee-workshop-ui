8f0a49efe3055042da03d071bf4eaa85effd85fc
Tarun Jaiswal <tarunjaiswal2020@gmail.com>
2026-04-06 22:04:05 +0530

    Just Finished setting up the react router and create the home section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
-import Hero from './Hero';
-import React from 'react';
+import React from'react';
+import {BrowserRouter as Router,Routes,Route}from'react-router-dom';
+import Home from './Home';
 import Navbar from './Navbar';
-function App() {
-  return (
-    <div>
+function App(){
+  return(
+    <Router>
       <Navbar/>
-      <Hero/>
-      <div className="py-14 px-6 text-center bg-white">
-      <h2 className="text-3xl font-bold text-gray-800 mb-3">Why FOSSEE?</h2>
-      <p className="text-gray-900 max-w-2xl mx-auto text-base leading-relaxed">
-       FOSSEE (Free/Libre and Open Source Software for Education) promotes open-source tools to improve educational quality and reduce dependence on expensive proprietary software in Indian academia. It offers free, industry-relevant training, workshops, and internships to students and faculty, funded by the Ministry of Education, Government of India.
-      </p>
-      </div>
-    </div>);
-}
-export default App;
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>);}export default App;
---
+++ frontend/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+const Home=()=>{
+  return(
+    <div>
+      <Hero/>
+      <div className="py-14 px-6 text-center bg-white">
+        <h2 className="text-3xl font-bold text-gray-800 mb-3">Why FOSSEE?</h2>
+        <p className="text-gray-900 max-w-2xl mx-auto text-base leading-relaxed">
+        FOSSEE (Free/Libre and Open Source Software for Education) promotes open-source tools to improve educational quality and reduce dependence on expensive proprietary software in Indian academia. 
+        It offers free, industry-relevant training, workshops, and internships to students and faculty, funded by the Ministry of Education, Government of India.
+        </p>
+       </div>
+    </div>);};export default Home;
